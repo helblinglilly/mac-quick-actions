5e845e7e6b2cff63dfbbf9fe91a13a646e0e6bd0
Lilly Helbling <lilly@helbling.uk>
2024-03-29 22:59:22 +0000

    Support multiple variables

--- setup.sh
+++ setup.sh
@@ -24,10 +24,11 @@ for action in workspace/* ; do
 
   # Pipe the parsed output into a temp location - document.wflow would otherwise 
   # just be an empty file - not sure why
-  sed "s|@@FFMPEGLOCATION@@|$(which 'ffmpeg')|g" "$fileWithPlaceholders" > parsed.txt
-  sed "s|@@VSCODELOCATION@@|$(which 'code')|g" "$fileWithPlaceholders" > parsed.txt
-  rm "$fileWithPlaceholders"
-  mv parsed.txt "$fileWithPlaceholders"
+
+  sed "s|@@FFMPEGLOCATION@@|$(which 'ffmpeg')|g" "$fileWithPlaceholders" > $fileWithPlaceholders-edited
+  mv "$fileWithPlaceholders-edited" "$fileWithPlaceholders"
+  sed "s|@@VSCODELOCATION@@|$(which 'code')|g" "$fileWithPlaceholders" > $fileWithPlaceholders-edited
+  mv "$fileWithPlaceholders-edited" "$fileWithPlaceholders"
 
   # echo "Removed previous versions of actions"
   rm -fr "$targetLocation/$actionName" || true
